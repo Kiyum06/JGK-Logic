e7eacf9a936a331d2e79cde613dab2b611e21049
JPbooya <perezjcarlos81@gmail.com>
2026-06-11 18:29:06 -0700

    implemented login form

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import { registerUser } from "../services/auth.service.js";
-
+// renders the form
 export const getRegister = (req,res) => {
     res.render('register', {title: 'Register', subtitle: '', error: null});
 };
 
+// handles registeration form submission
 export const postRegister = async (req,res) => {
     const {username, email, password } = req.body;
     try {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { createUser, findUserByEmail } from '../model/default.repo.js';
 
+// hashes password with bcrypt and saves the user to DB
 export const registerUser = async (username, email, password) => {
     const existing = await findUserByEmail(email);
     if (existing) {
