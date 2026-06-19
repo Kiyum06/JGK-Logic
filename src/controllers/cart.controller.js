b021945fada2860ff125d9dbf8fd028beb30052d
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-06-18 17:37:13 -0700

    added cart item endpoint

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -8,4 +8,25 @@ export const getCart = (req, res) => {
     });
 };
 
- 
+export const addItemToCart = (req, res) => {
+    const { productId } = req.body;
+
+    if (!productId) {
+        return res.status(400).json({
+            success: false,
+            message: "Product Id is required"
+        });
+    }
+
+    if (!req.session.cart) {
+        req.session.cart = [];
+    }
+
+    req.session.cart.push({ productId });
+
+    res.status(201).json({
+        success: true,
+        count: req.session.cart.length,
+        data: req.session.cart
+    });
+};
--- src/routers/cart.routes.js
+++ src/routers/cart.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { requireAuthApi } from "../utility/default.util.js";
-import { getCart,} from "../controllers/cart.controller.js";
- 
+import { getCart, addItemToCart } from "../controllers/cart.controller.js"; 
 
 const router = Router();
 
 router.get("/api/cart", requireAuthApi, getCart);
+router.post("/api/cart/items", requireAuthApi, addItemToCart);
 
 
 export default router;
